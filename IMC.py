c0c3981974eb2d4154eef2f42e7e60007a6d3ab9
Datrisa <datrisaoficial@gmail.com>
2021-04-23 12:04:02 -0300

    IMC por faixa, 8 faixas, com tabela IMC, para cmd

--- IMC.py
+++ IMC.py
@@ -1,4 +1,4 @@
-# Calculo de IMC - Indice de Massa Corporal com tabela básica
+        # Calculo de IMC - Indice de Massa Corporal com tabela básica
 nome=input('Digite seu Nome:\n ').upper()
 print('Olá',nome, 'Você é benvindo!! \nPreciso de informações para calcular seu IMC,\nPor favor utilize PONTO e não vírgula')
 peso=float(input('  Qual seu Peso? \n'))
@@ -7,7 +7,7 @@ resultado=(peso/(altura*altura))
 print('{:.2f}'.format(resultado))
 print('{} Seu ICM é: {:.2f}'.format(nome,resultado))
 
-# tabela comprativa
+        # tabela progressiva
 if (resultado <= 16):
     print('     Magreza Grave, tem que se cuidar\n')
 elif (resultado <=17) :
@@ -25,7 +25,7 @@ elif (resultado <= 40):
 else:
     print('     Obesidade Grau 3, está precisando emagrecer urgente\n')
 
-# visualização da tabela em um PopUp
+        # visualização da tabela em um PopUp
 tabela = input('Deseja ver nossa Tabela de IMC?? S ou N \n').upper()
 if (tabela == "S"):
     print('TABELA DE IMC (KG/M²):\n'
@@ -41,8 +41,13 @@ if (tabela == "S"):
           '     \n'
 
                     'Obrigado por Participar!!!')
+
 else:
     print('Agradecemos sua participação')
+
+        # somente para executar em CMD-python
+input("Pressione  ENTER para Sair")
+
 
 
 
